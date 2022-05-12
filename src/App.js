4755d5ea7653f9f7072126c07ecc2ec6a8e23326
thenavik <iamdigitalist@gmail.com>
2022-05-12 16:33:01 +0300

    frame animation 1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout
+        onClick={() => setIsOpen(!isOpen)}
+        className="card"
+        style={{
+          borderRadius: "2rem",
+          boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <motion.h2 layout="position">Frame Animation</motion.h2>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="expand"
+          >
+            <p>
+              The standard chunk of Lorem Ipsum used since the 1500s is
+              reproduced below for those interested.
+            </p>
+            <p>Sections 1.10.32 and 1.10.33 from</p>
+            <button>Read More</button>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
